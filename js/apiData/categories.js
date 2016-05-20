72b558f8b48ce2e2b0a2b3e419621c1edf08f923
jeimer3184@gmail.com <jeimer3184@gmail.com>
2016-05-19 21:09:56 -0500

    First commit, adding existing work. Still WIP

---
+++ js/apiData/categories.js
@@ -0,0 +1,33 @@
+var exampleArray =
+[
+{
+id: 10044,
+title: "classic lit",
+clues_count: 10
+},
+{
+id: 11507,
+title: "bay o' wolf",
+clues_count: 5
+},
+{
+id: 11508,
+title: "gullible travels",
+clues_count: 5
+},
+{
+id: 11509,
+title: "little women",
+clues_count: 5
+},
+{
+id: 11510,
+title: "pair of dice, lost",
+clues_count: 5
+},
+{
+id: 11511,
+title: "the scarlet letters",
+clues_count: 5
+}
+]
